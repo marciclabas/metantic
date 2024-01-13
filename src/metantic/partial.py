91ad03dbe2cd0a5d23502df1e058a3811b8324cf
m4rs <marcel@marcelclaramunt.cat>
2024-01-13 12:12:12 +0100

    partial v2 (now works with sub-classes)

--- src/metantic/partial.py
+++ src/metantic/partial.py
@@ -6,7 +6,7 @@ Model = TypeVar('Model', bound=BaseModel)
 def Partial(model: type[Model]) -> type[Model]:
     """`model` but with all fields optional"""
     fields = {
-        f: (t | None, None)
-        for f, t in model.__annotations__.items()
+        f: (a.annotation | None, None)
+        for f, a in model.model_fields.items()
     }
     return create_model(f'Partial{model.__name__}', __base__=model, **fields)
