003c8abf55d4bee0d7c8e4d2402ff509dbfb34dd
m4rs <marcel@marcelclaramunt.cat>
2024-01-15 20:45:02 +0100

    config, no base

--- src/metantic/omit.py
+++ src/metantic/omit.py
@@ -1,13 +1,15 @@
 from typing import Literal, TypeVar
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, create_model, ConfigDict
 
 Model = TypeVar('Model', bound=BaseModel)
 
-def Omit(model: Model, fields: list[str]) -> type[Model]:
+def Omit(model: type[Model], fields: list[str], name: str | None = None) -> type[Model]:
     """`model` without `fields`"""
     keep_fields = {
         f: (a.annotation, a.default)
         for f, a in model.model_fields.items()
             if f not in fields
     }
-    return create_model(f'Omit{model.__name__}', **keep_fields)
+    name = name or f'Omit{model.__name__}'
+    config = ConfigDict(extra="forbid")
+    return create_model(name, __config__=config, **keep_fields)
--- src/metantic/partial.py
+++ src/metantic/partial.py
@@ -3,10 +3,11 @@ from pydantic import BaseModel, create_model
 
 Model = TypeVar('Model', bound=BaseModel)
 
-def Partial(model: type[Model]) -> type[Model]:
+def Partial(model: type[Model], name: str | None = None) -> type[Model]:
     """`model` but with all fields optional"""
     fields = {
         f: (a.annotation | None, None)
         for f, a in model.model_fields.items()
     }
-    return create_model(f'Partial{model.__name__}', __base__=model, **fields)
+    name = name or f'Partial{model.__name__}'
+    return create_model(name, __config__=model.model_config, **fields)
