18a8f71172e5c03b8372703600d9abbd72b57b17
m4rs <marcel@marcelclaramunt.cat>
2024-01-22 16:28:22 +0100

    Take with generic types

--- src/metantic/take.py
+++ src/metantic/take.py
@@ -1,8 +1,9 @@
-from typing import TypeVar, Any
+from typing import TypeVar, Any, get_args, get_origin
 from pydantic import BaseModel, ConfigDict, create_model
+import ramda as R
 
 Model = TypeVar('Model', bound=BaseModel)
-def Take(model: type[Model], fields: list[str | tuple[str, list]]) -> Model:
+def Take(model: type[Model], fields: list[str | tuple[str, list]]) -> type[Model]:
     """`model` with `fields` only
     - if `fields[i]` is a `str`, takes only that field
     - if `fields[i]` is a `tuple[str, list]`, takes that subfields recursively
@@ -25,6 +26,9 @@ def Take(model: type[Model], fields: list[str | tuple[str, list]]) -> Model:
     })
     ```
     """
+    args = get_args(model)
+    if len(args) > 0 and not issubclass(get_origin(model), BaseModel):
+        model = R.find(lambda a: issubclass(a, BaseModel), args)
     def field(selector: str | tuple[str, list]) -> tuple[str, tuple[type, Any]]:
         match selector:
             case str(fld):
