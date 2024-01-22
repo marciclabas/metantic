aa9f208529229444a8f4ff4c21ce75e6a131e209
m4rs <marcel@marcelclaramunt.cat>
2024-01-22 18:20:39 +0100

    oops missing take import

--- src/metantic/__init__.py
+++ src/metantic/__init__.py
@@ -2,7 +2,7 @@ from .util import meta_map
 from .partial import Partial
 from .fields import Fields
 from .omit import Omit
-# from .take import Take
+from .take import Take
 from . import paths
 try:
     from . import fastapi
