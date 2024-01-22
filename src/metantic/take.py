55d7d2aa5e0ff5c18f4b9fcda3e5a99b28cfc94e
m4rs <marcel@marcelclaramunt.cat>
2024-01-22 18:18:50 +0100

    better Take

--- src/metantic/take.py
+++ src/metantic/take.py
@@ -1,10 +1,9 @@
-from typing import TypeVar, Any, get_args, get_origin
-from pydantic import BaseModel, ConfigDict, create_model
+from typing import GenericAlias, TypeVar, Any, get_args, get_origin
+from pydantic import BaseModel, create_model, ConfigDict
 import ramda as R
-from . import model_type
 
 Model = TypeVar('Model', bound=BaseModel)
-def Take(model: type[Model], fields: list[str | tuple[str, list]]) -> type[Model]:
+def Take(cls: type[Any], fields: list[str | tuple[str, list]]) -> type[Model]:
     """`model` with `fields` only
     - if `fields[i]` is a `str`, takes only that field
     - if `fields[i]` is a `tuple[str, list]`, takes that subfields recursively
@@ -27,20 +26,26 @@ def Take(model: type[Model], fields: list[str | tuple[str, list]]) -> type[Model
     })
     ```
     """
-    model_t = model_type(model)
-    assert model_t is not None, f"Invalid model '{model}' in subtype hierarchy"
-    def field(selector: str | tuple[str, list]) -> tuple[str, tuple[type, Any]]:
-        match selector:
-            case str(fld):
-                f = model_t.model_fields[fld]
-                return fld, (f.annotation, f.default)
-            case str(fld), list(subflds):
-                f = model_t.model_fields[fld]
-                return fld, (Take(f.annotation, subflds), f.default)
-            case _:
-                raise ValueError(f"Unexpected field selector '{selector}' (of type ({type(selector)})) (expected `str | tuple[str, list]`)")
-            
-    keep_fields = dict(field(s) for s in fields)
-    name = f'Take{model_t.__name__}'
-    config = ConfigDict(extra="forbid")
-    return create_model(name, __config__=config, **keep_fields)
+    if isinstance(cls, GenericAlias):
+        base = get_origin(cls)
+        args = get_args(cls)
+        mapped_args = tuple(R.map(Take(fields=fields), args))
+        return base[mapped_args]
+    elif issubclass(cls, BaseModel):
+        def field(selector: str | tuple[str, list]) -> tuple[str, tuple[type, Any]]:
+            match selector:
+                case str(fld):
+                    f = cls.model_fields[fld]
+                    return fld, (f.annotation, f.default)
+                case str(fld), list(subflds):
+                    f = cls.model_fields[fld]
+                    return fld, (Take(f.annotation, subflds), f.default)
+                case _:
+                    raise ValueError(f"Unexpected field selector '{selector}' (of type ({type(selector)})) (expected `str | tuple[str, list]`)")
+
+        keep_fields = dict(field(s) for s in fields)
+        name = f'Take{cls.__name__}'
+        config = ConfigDict(extra="forbid")
+        return create_model(name, __config__=config, **keep_fields)
+    else:
+        return cls
